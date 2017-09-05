a02816b0664cd69385853838c2fab8c9892e57f9
Marcin Czachurski <mczachurski@icloud.com>
2017-09-05 10:02:04 +0200

    Move client address to appsettings.file

--- test/Neutrino.Client.Specs/NeutrinoClientFactory.cs
+++ test/Neutrino.Client.Specs/NeutrinoClientFactory.cs
@@ -1,14 +1,23 @@
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
 namespace Neutrino.Client.Specs
 {
     public static class NeutrinoClientFactory
     {
         public static NeutrinoClient GetNeutrinoClient()
         {
-            var serverAddress = "http://localhost:5000";
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            var configuration = builder.Build();
+
+            var serverAddress = configuration["ClientAddress"];
 
             var neutrinoClientOptions = new NeutrinoClientOptions();
             neutrinoClientOptions.Addresses = new string[] { serverAddress };
-			neutrinoClientOptions.SecureToken = "4e57e961-5f2e-4b24-893f-7842c5ccff97";
+			neutrinoClientOptions.SecureToken = configuration["SecureToken"];
 
 			var httpRequestService = new HttpRequestService(neutrinoClientOptions);
 			return new NeutrinoClient(httpRequestService);
